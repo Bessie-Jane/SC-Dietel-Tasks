9ea384af48e2a15e113c3f413ebfcb66d7eb76c8
Bessie-Jane Kalu <OlammaKalu007@gmail.com>
2021-06-11 11:24:52 +0100

    Fibonacci Task

---
+++ src/chapterSeven/Fibonacci.java
@@ -0,0 +1,19 @@
+package chapterSeven;
+
+public class Fibonacci {
+
+    public  static int fibonacci(int n){
+
+        int[]fibSeries = new int[n];
+
+        fibSeries[1] = 1;
+
+        for(int x = 2; x < n; x++){
+            fibSeries[x] = fibSeries[x - 1] + fibSeries[x-2];
+        }
+        return fibSeries[n-1];
+    }
+
+
+
+}
